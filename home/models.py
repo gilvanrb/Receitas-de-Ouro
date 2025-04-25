5c5b271cf969b51922ccfafc1f80cd7be49e865a
gilvanrb <gilvanrb.barros@gmail.com>
2025-04-25 20:55:31 -0300

    Versão 4
    Adição de receitas

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Receitas(models.Model):  
     titulo = models.CharField(max_length=50)
-    descricao = models.TextField(max_length=150)
+    descricao = models.TextField(max_length=300)
     ingredientes = models.TextField()
     preparo = models.TextField()
     imagem = models.ImageField(upload_to='recetas/', null = True, blank=True)
